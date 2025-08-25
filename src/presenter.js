4d48fb8d8d48123ee08ba5343ab37021960895ea
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-08-25 11:51:13 -0400

    feat(saludoNomHoragenEdad.js): Agrego nueva funcionalidad modificando el presenter e index.html

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,8 @@
-import saludar from "./saludoNomHoraGen.js";
+import saludar from "./saludoNomHoragenEdad.js";
 
 const nom = document.querySelector("#nombre");
 const genero = document.querySelector("#genero");
+const edad = document.querySelector("#edad");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -10,6 +11,7 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nom.value.trim(); // trim opcional para limpiar posibles espacios
   const gen = genero.value;
+  const anios = Number.parseInt(edad.value);
 
-  div.innerHTML = "<p>" + saludar(nombre, gen) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, gen, anios) + "</p>";
 });
---
+++ src/saludoNomHoragenEdad.js
@@ -0,0 +1,25 @@
+function saludar(nombre, gen, edad) {
+
+    let fechaActual = new Date();
+    let horaActual = fechaActual.getHours();
+    let saludo = "";
+    let trato = "";
+
+    if (horaActual >= 6 && horaActual < 12) {        
+        saludo = "Buenos dias, ";                   
+    } else if (horaActual >= 12 && horaActual < 19) {
+        saludo = "Buenas tardes, ";                 
+    } else {
+        saludo = "Buenas noches, ";                 
+    }
+
+    trato = gen === "M" ? "mi estimado " : "mi estimada ";
+
+    if(edad > 30){
+        trato += gen === "M" ? "señor " : "señora ";
+    }
+    
+    return saludo + trato + nombre;
+}
+
+export default saludar;

972afaeab514eb8d0f4953407d0f543791cfb422
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-08-25 12:15:16 -0400

    feat(saludoNomHoragenEdadIdioma.js): Agrego nueva funcionalidad modificando el presenter e index.html

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,9 @@
-import saludar from "./saludoNomHoragenEdad.js";
+import saludar from "./saludoNomHoraGenEdadIdioma.js";
 
 const nom = document.querySelector("#nombre");
 const genero = document.querySelector("#genero");
 const edad = document.querySelector("#edad");
+const idioma = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -12,6 +13,7 @@ form.addEventListener("submit", (event) => {
   const nombre = nom.value.trim(); // trim opcional para limpiar posibles espacios
   const gen = genero.value;
   const anios = Number.parseInt(edad.value);
+  const lang = idioma.value;
 
-  div.innerHTML = "<p>" + saludar(nombre, gen, anios) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, gen, anios, lang) + "</p>";
 });

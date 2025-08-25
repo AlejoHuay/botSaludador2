d94238fd04e531fe65670b42e36718b186daa02d
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-08-25 16:40:17 -0400

    agrego comentarios y modifico de ejemplo saludoNomHora.test.js para mayor claridad del porque del codigo en las pruebas

--- src/saludoNomHora.test.js
+++ src/saludoNomHora.test.js
@@ -1,29 +1,34 @@
 import saludar from "./saludoNomHora.js";
 
 describe("saludoNomHora", () => {
-  beforeAll(() => {
-    jest.useFakeTimers();
+  beforeAll(() => { //beforeAll y afterAll → solo una vez para toda la suite (describe).
+    jest.useFakeTimers(); // A partir de ahora no uses el reloj real del sistema, usa un reloj simulado controlado por Jest.
   });
   afterAll(() => {
-    jest.useRealTimers();
+    jest.useRealTimers(); // Al final de la suite de pruebas deja de simular, usa el reloj real otra vez.
   });
 
   it("debería saludar en la mañana (Buenos días)", () => {
-    jest.setSystemTime(new Date("2025-01-01T09:00:00")); // 09:00
-    const out = saludar("Alejo");
-    expect(out).toContain("Buenos"); // evita problemas de tildes
-    expect(out).toContain("Alejo");
+    jest.setSystemTime(new Date("2025-01-01T09:00:00")); // Haz como si la fecha actual fuera tal fecha y tal hora 09:00, Funciona solo en modo fake timers.
+    expect(saludar("Alejo")).toBe("Buenos dias, Alejo");
   });
 
   it("debería saludar en la tarde (Buenas tardes)", () => {
     jest.setSystemTime(new Date("2025-01-01T15:00:00")); // 15:00
-    const out = saludar("Alejo");
-    expect(out).toContain("tardes");
+    expect(saludar("Alejo")).toBe("Buenas tardes, Alejo");
   });
 
   it("debería saludar en la noche (Buenas noches)", () => {
     jest.setSystemTime(new Date("2025-01-01T21:00:00")); // 21:00
-    const out = saludar("Alejo");
-    expect(out).toContain("noches");
+    
+    //toBe -> más estricto, asegura que la cadena sea exactamente lo que esperas.
+    expect(saludar("Alejo")).toBe("Buenas noches, Alejo");
+
+    //toContain -> más flexible, asegura solo que esté la parte importante. Con const si pensamos hacer varios .toContain 
+    //const out = saludar("Alejo");
+    //expect(out).toContain("noches");
+
+    // con una sola espectativa 
+    //expect(saludar("Alejo")).toContain("noches");
   });
 });

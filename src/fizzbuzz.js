488ffdf75f8818e340777ec89dbe81d5feb94099
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-08-26 10:26:32 -0400

    feat: convierto 3 y cualquier numero  multiplo de 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,7 @@
 function convertirFizzBuzz(n){
+    if(n % 3 === 0){
+        return "Fizz";
+    }
     return n + "";
 }
 export default convertirFizzBuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,10 +1,18 @@
 import convertirFizzBuzz from "./fizzbuzz";
 
 describe("Fizzbuzz", () => {
+  
   it("deberia convertir un numero que no es multiplo de 3 ni 5", () => {
     expect(convertirFizzBuzz(1)).toEqual("1");
   });
+
   it("deberia convertir cualquier numero que no es multiplo de 3 ni 5", () => {
     expect(convertirFizzBuzz(4)).toEqual("4");
   });
+
+  it("deberia convertir numero 3 a Fizz", () => {
+    expect(convertirFizzBuzz(3)).toEqual("Fizz");
+  });
+
+
 });
